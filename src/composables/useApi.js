98ff39368318434fc27f629b2be4a219ff98e649
xiaoxing.sn <xiaoxing.sn@bytedance.com>
2026-03-12 21:56:30 +0800

    feat: init

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -4,8 +4,8 @@ export function useApi() {
   const { state } = useAuth()
 
   async function api(endpoint, { method = 'GET', body, query } = {}) {
-    const base = import.meta.env.BASE_URL.replace(/\/$/, '')
-    let url = base + '/api' + endpoint
+    const pathBase = window.location.pathname.replace(/\/?(?:#.*)?$/, '')
+    let url = pathBase + '/api' + endpoint
     if (query) {
       const qs = new URLSearchParams(query).toString()
       if (qs) url += '?' + qs
