54dc8bc53ed107e6594b02ca0b35ea510c56509e
xiaoxing.sn <xiaoxing.sn@bytedance.com>
2026-03-12 21:47:51 +0800

    feat: init

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuth } from '../composables/useAuth'
 
 import LoginView from '../views/LoginView.vue'
@@ -34,7 +34,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 })
 
